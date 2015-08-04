2c43b24a08a154cbf06f5ed051bc25ee08ec8471
Terri Burns <terri.burns@nyu.edu>
2015-08-04 09:20:20 -0400

    Update all references and links to Tumblr

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
       d = document;
 
       var f = d.createElement('form');
-      f.action = 'http://gtmetrix.com/analyze.html?bm';
+      f.action = 'http://tumblr.com/';
       f.method = 'post';
       var i = d.createElement('input');
       i.type = 'hidden';
