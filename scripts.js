c61d2b17adf769f43cf8eaaad9b14109b080abc9
Terri Burns <terri.burns@nyu.edu>
2015-08-11 09:42:44 -0400

    Changed extension link to be a direct posting link

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
       d = document;
 
       var f = d.createElement('form');
-      f.action = 'http://tumblr.com/';
+      f.action = 'https://www.tumblr.com/new/text';
       f.method = 'post';
       var i = d.createElement('input');
       i.type = 'hidden';
