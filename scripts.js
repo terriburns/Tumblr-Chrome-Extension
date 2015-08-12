56cd7a15e409731fd4a7d32478c6410cb37a00ac
Terri Burns <terri.burns@nyu.edu>
2015-08-12 09:06:00 -0400

    Updated links, styles, and icons

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
       d = document;
 
       var f = d.createElement('form');
-      f.action = 'https://www.tumblr.com/new/text';
+      f.action = 'http://tumblr.com';
       f.method = 'post';
       var i = d.createElement('input');
       i.type = 'hidden';
